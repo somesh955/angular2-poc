9a3a51a352ad9d9f4fbc066b48878a226a774045
somesh955 <somesh955@gmail.com>
2017-02-18 23:11:06 +0530

    pre set module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { HttpModule } from '@angular/http';
 
+import './core/rxjs-extensions';
+//import { AppRoutingModule } from './app-routing.module';
+//import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { PageNotFoundComponent } from './page-not-found.component';
 import { AppComponent }  from './app.component';
 
+/* Feature Modules */
+//import { CoreModule } from './core/core.module';
+//import { LoginModule } from './login/login.module';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+  //  HttpModule,
+  //  LoginModule,
+  //  AppRoutingModule,
+  //  CoreModule
+    ],
+  declarations: [ AppComponent, PageNotFoundComponent ],
+  providers: [],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
